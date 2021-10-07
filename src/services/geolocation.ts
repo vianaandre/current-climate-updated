77be6aa7fedcf7499c94845d10b59de91e17c202
vianaandre <andredapperviana@gmail.com>
2021-10-07 18:26:17 -0300

    fix: (modification return function)

--- src/services/geolocation.ts
+++ src/services/geolocation.ts
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const geolocation = async () => {
+const geolocation = async (lat: number, lon: number) => {
   const response = await axios('https://api.openweathermap.org/geo/1.0/reverse', {
     method: 'GET',
     params: {
-      lat: '-24.8426',
-      lon: '-54.324',
+      lat,
+      lon,
       lang: 'pt_br',
       appid: '005bae086b2f834f591f43fad4011bb5',
     },
   });
 
-  console.log(response.data[0]);
+  return response.data[0];
 };
 
 export default geolocation;
--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const weather = async () => {
+const weather = async (lat: number, lon: number) => {
   const response = await axios('https://api.openweathermap.org/data/2.5/onecall', {
     method: 'GET',
     params: {
-      lat: '-24.8426',
-      lon: '-54.324',
+      lat,
+      lon,
       lang: 'pt_br',
       units: 'metric',
       appid: '005bae086b2f834f591f43fad4011bb5',
     },
   });
 
-  console.log(response.data);
+  return response.data;
 };
 
 export default weather;
